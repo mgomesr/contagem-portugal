d3a48a7c7b41f8c384e3ec464a5a84f6ab90af83
Matheus Gomes Rodrigues <136190435+mgomesr@users.noreply.github.com>
2023-07-28 17:00:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var data1 = new Date('09/01/2023');
+var data1 = new Date('08/15/2023');
 var description1 = document.querySelector('.description-1');
 
-var data2 = new Date('10/01/2023');
+var data2 = new Date('08/30/2023');
 var description2 = document.querySelector('.description-2');
 
-var data3 = new Date('10/31/2023');
+var data3 = new Date('09/30/2023');
 var description3 = document.querySelector('.description-3');
 
-var data4 = new Date('11/25/2023');
+var data4 = new Date('10/22/2023');
 var description4 = document.querySelector('.description-4');
 
 var hoje = new Date();
